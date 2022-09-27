3f658818493bdcf07b4363439b3345875e1b3eca
Liam McCarthy <ltmccarthy9@gmail.com>
2022-09-27 15:48:15 -0500

    signup

---
+++ client/src/components/Signup.js
@@ -0,0 +1,16 @@
+
+const Signup = () => {
+    return (
+        <div>
+            <h1>Signup</h1>
+            <form>
+                <input type="text" placeholder="First Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <input type="submit" value="Signup" />
+            </form>
+        </div>
+    );
+}
+
+export default Signup;

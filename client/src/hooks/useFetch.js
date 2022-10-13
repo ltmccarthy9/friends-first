de42d6b31d72a641c681d74228132036ad6621a1
Liam McCarthy <ltmccarthy9@gmail.com>
2022-10-13 13:03:51 -0500

    use fetch axios

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react"
 
+// This is a custom hook that we will call when data needs to populate
+// a page on page load.  
+
+// 
 const useFetch = (url) => {
+    // use states for our data, loading, and errors
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState([]);
     const [error, setError] = useState(false);
 
+    // useEffect triggers on page load
+    // axios fetches our database
+    // for example, when we open dashboard, this will be called and 
+    // then we will use the returned data to populate the page
     useEffect (() => {
         const fetchData = async () => {
             setLoading(true);
             try {
-                const res = await fetch(url, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
+                const res = await axios.get(url)
+                setData(res.data);
             } catch(err) {
                 setError(err);
             }
@@ -27,12 +32,8 @@ const useFetch = (url) => {
 const reFetch = async () => {
     setLoading(true);
     try {
-        const res = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        const res = await axios.get(url)
+        setData(res.data);
     } catch(err) {
         setError(err);
     }
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 
 const Dashboard = () => {
     const navigate = useNavigate();
+
+    const {data, loading, error } = useFetch("");
 
     // Check to see if the user is logged in
     useEffect(() => {

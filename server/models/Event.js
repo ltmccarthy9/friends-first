c4c952165fb7b01f42d5b63d35557fb5d191a44a
Liam McCarthy <ltmccarthy9@gmail.com>
2022-11-26 23:17:29 -0600

    date added to event model

--- server/models/Event.js
+++ server/models/Event.js
@@ -24,7 +24,22 @@ const eventSchema = new mongoose.Schema({
     category: {
         type: String,
         required: true,
-    }
+    },
+    date: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return(
+                    v &&
+                    v.getTime() > Date.now() + 24 * 60 * 60 * 1000 &&
+                    v.getTime() < Date.now() + 90 * 24 * 60 * 60 * 1000
+                );
+            },
+            message: "Event must be at least one day from now and no more than ninety days out"
+        }
+        
+    },
 });
 
 // we will need to add a date and time and auto remove from active events
